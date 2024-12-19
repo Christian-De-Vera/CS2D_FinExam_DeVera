99a11ead858ea385501acac8ded9f43264c9fd10
Christian <zen.chishiki404@gmail.com>
2024-12-19 21:17:35 +0800

    ...

--- mdArray.js
+++ mdArray.js
@@ -3,7 +3,7 @@ let ages = prompt(`24, 65, 21, 5, 9`).split(`,`);
 
 // the multi-dimensional array
 for (let i = 0; i < names.length; i++) {
-    // Trim whitespace from each name and age, and convert the age to a number
+    // remove whitespace from each name and age, and convert the age to a number
     let name = names[i].trim();       // Remove spaces from the name
     let age = parseInt(ages[i].trim()); // Convert age from a string to a number
 
@@ -11,7 +11,7 @@ for (let i = 0; i < names.length; i++) {
     multiDimensionalArray.push([name, age]);
 }
 
-// Log the restructured array to the console
+// Log the restructured array
 for (let i = 0; i < multiDimensionalArray.length; i++) {
     console.log(multiDimensionalArray[i]);
 }
